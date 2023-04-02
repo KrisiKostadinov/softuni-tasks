f5e88a8f482bc0018c07e1a559163404d646907b
krisikostadinov <krisi.199898@gmail.com>
2023-04-02 13:29:45 +0300

    "Negative / Positive Numbers" Task Solved!

---
+++ JS-Advanced/Exercises/Arrays and Nested Arrays - Lab/04.js
@@ -0,0 +1,15 @@
+function solution(array = []) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            result.unshift(array[i]);
+        } else {
+            result.push(array[i]);
+        }
+    }
+
+    console.log(result.join('\n'));
+}
+solution([7, -2, 8, 9]);
+solution([3, -2, 0, -1]);
