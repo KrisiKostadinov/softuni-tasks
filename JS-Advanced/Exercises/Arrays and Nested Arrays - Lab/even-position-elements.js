4902b663e58f1a7debd39e82e679f128ecb75730
krisikostadinov <krisi.199898@gmail.com>
2023-04-02 12:55:40 +0300

    "Even Position Elements" Task Solved!

---
+++ JS-Advanced/Exercises/Arrays and Nested Arrays - Lab/even-position-elements.js
@@ -0,0 +1,8 @@
+function solution(array = []) {
+  const filteredElements = array.filter((element, index) => {
+    return index % 2 === 0;
+  });
+  console.log(filteredElements.join(" "));
+}
+solution(["20", "30", "40", "50", "60"]);
+solution(["5", "10"]);
